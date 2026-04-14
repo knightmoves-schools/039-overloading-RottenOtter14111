6722cb918778958abecbad2315586dedc6b6e91b
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-04-14 17:06:05 +0000

    done

--- Person.cs
+++ Person.cs
@@ -1,9 +1,22 @@
 ﻿namespace knightmoves;
 
-public class Person{
-   public string FirstName;
-   public string LastName;
-   public int Age;
+   public class Person(String firstName, String lastName, int age)      
+         {public string firstName;
+         public string lastName;
+         public int age;
 
-   //Add your code here
+         public Person(String firstName, String lastName, int age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+      
+   }
+
+   public Person(String firstName, String lastName){
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+      
+  
+       
 }
